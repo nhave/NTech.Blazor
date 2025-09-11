495dcea6123a1806c6b7f6c907bfbfa22b1ef7fe
Nikolaj Have <nhave06@gmail.com>
2025-09-11 17:31:15 +0200

    Added Nav Tabs.

---
+++ NTech.Blazor/Helpers/ClassBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace NTech.Blazor
+{
+    public static class ClassBuilder
+    {
+        public static string Build(params string[] classes)
+        {
+            return string.Join(" ", classes.Where(c => !string.IsNullOrWhiteSpace(c)));
+        }
+    }
+}
